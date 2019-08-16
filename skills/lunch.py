7a7b379200ae75cf458734a309f884f17ccec420
TTEarth <kyunglaw91@gmail.com>
2019-08-17 00:59:56 +0900

    Update lunch.py
    random 함수를 get_lunch 함수 안에서 실행하도록 소스 코드를 옮김

--- skills/lunch.py
+++ skills/lunch.py
@@ -19,11 +19,11 @@ from skills import add_skill
 
 import random
 foods = ['자장면', '짬뽕', '탕수육', '잡채', '떡볶이', '라면', '한식', '돈가스', '김밥', '국수', '김치찌개', '된장찌개', '구내식당', '피자', '햄버거', '비빔밥', '불고기', '삼겹살', '치킨', '갈비', '생선구이', '냉면', '국밥', '삼계탕', '순두부찌개', '파전', '죽']
-food = random.choice(foods)
-presenter.remove(food)
 
 @add_skill(r'점심')
 def get_lunch(message):
+    food = random.choice(foods)
+    presenter.remove(food)
     return LunchSendMessage(
         title='점심',
         lunch=food
